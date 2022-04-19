12c2635cc5f0abee0f64cc94621114a846efb329
Emanuel <emanueljuricev5@gmail.com>
2022-04-19 18:00:58 +0200

    level and camera design

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -23,22 +23,37 @@ public class CameraFollow : MonoBehaviour
 
     //Enable and set the maximum X value
     public bool XMaxEnabled = false;
-    public float XMaxValue = 0;       
+    public float XMaxValue = 0;
 
     //Enable and set the min X value
     public bool XMinEnabled = false;
     public float XMinValue = 0;
 
-    void FixedUpdate(){
+    void FixedUpdate()
+    {
         //Target position
         Vector3 targetPos = target.position;
 
         //Vertical
         if (YMinEnabled && YMaxEnabled)
             targetPos.y = Mathf.Clamp(target.position.y, YMinValue, YMaxValue);
-        
+
+        else if (YMinEnabled)
+            targetPos.y = Mathf.Clamp(target.position.y, YMinValue, target.position.y);
+
+        else if (YMaxEnabled)
+            targetPos.y = Mathf.Clamp(target.position.y, target.position.y, YMaxValue);
+
 
         //Horizontal
+        if (XMinEnabled && XMaxEnabled)
+            targetPos.x = Mathf.Clamp(target.position.x, XMinValue, XMaxValue);
+
+        else if (XMinEnabled)
+            targetPos.x = Mathf.Clamp(target.position.x, XMinValue, target.position.x);
+
+        else if (XMaxEnabled)
+            targetPos.x = Mathf.Clamp(target.position.x, target.position.x, XMaxValue);
 
         //Align the camera and the targets z position
         targetPos.z = transform.position.z;
